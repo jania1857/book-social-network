1965823cc97774593715cd65af70c4d9f7103ca1
Hubert Janiuk <hubert.janiuk.2ti@gmail.com>
2024-09-06 21:26:00 +0200

    Frontend project creation

---
+++ book-network-api/src/main/java/pl/jania1857/booknetwork/config/OpenApiConfig.java
@@ -0,0 +1,54 @@
+package pl.jania1857.booknetwork.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "Hubert Janiuk",
+                        email = "hubert.janiuk.2ti@gmail.com"
+                ),
+                description = "OpenAPI documentation for Book Network Social API",
+                title = "OpenAPI specification - Jania1857",
+                version = "1.0.0",
+                license = @License(
+                        name = "No license"
+                ),
+                termsOfService = "Terms of service"
+        ),
+        servers = {
+                @Server(
+                        description = "Local ENV",
+                        url = "http://localhost:8088/api/v1"
+                ),
+                @Server(
+                        description = "Production ENV",
+                        url = "https://hubertjaniuk.dev/book-service-network"
+                )
+        },
+        security = {
+                @SecurityRequirement(
+                        name = "bearerAuth"
+                )
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT auth",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+}
