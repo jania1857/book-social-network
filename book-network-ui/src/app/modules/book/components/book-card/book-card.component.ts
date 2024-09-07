3e67a43a5d14aa36fb663a2a5a70191c9fe93576
Hubert Janiuk <hubert.janiuk.2ti@gmail.com>
2024-09-08 00:44:26 +0200

    92530 - a bit of implementations done - close to the end of project

---
+++ book-network-ui/src/app/modules/book/components/book-card/book-card.component.ts
@@ -0,0 +1,74 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {BookResponse} from "../../../../services/models/book-response";
+
+@Component({
+  selector: 'app-book-card',
+  templateUrl: './book-card.component.html',
+  styleUrls: ['./book-card.component.scss']
+})
+export class BookCardComponent {
+
+  private _book: BookResponse = {};
+  private _bookCover: String | undefined;
+  private _manage = false;
+
+  get book(): BookResponse {
+    return this._book;
+  }
+
+  @Input()
+  set book(value: BookResponse) {
+    this._book = value;
+  }
+
+  get bookCover(): String | undefined {
+    if (this._book.cover) {
+      return 'data:image/jpeg;base64,' + this._book.cover;
+    }
+    return 'https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png';
+  }
+
+  set bookCover(value: String | undefined) {
+    this._bookCover = value;
+  }
+
+  get manage(): boolean {
+    return this._manage;
+  }
+
+  @Input()
+  set manage(value: boolean) {
+    this._manage = value;
+  }
+
+  @Output() private share: EventEmitter<BookResponse> = new EventEmitter<BookResponse>();
+  @Output() private archive: EventEmitter<BookResponse> = new EventEmitter<BookResponse>();
+  @Output() private addToWaitingList: EventEmitter<BookResponse> = new EventEmitter<BookResponse>();
+  @Output() private borrow: EventEmitter<BookResponse> = new EventEmitter<BookResponse>();
+  @Output() private edit: EventEmitter<BookResponse> = new EventEmitter<BookResponse>();
+  @Output() private details: EventEmitter<BookResponse> = new EventEmitter<BookResponse>();
+
+  onShowDetails() {
+    this.details.emit(this._book);
+  }
+
+  onBorrow() {
+    this.borrow.emit(this._book);
+  }
+
+  onAddToWaitingList() {
+    this.addToWaitingList.emit(this._book);
+  }
+
+  onEdit() {
+    this.edit.emit(this._book);
+  }
+
+  onShare() {
+    this.share.emit(this._book);
+  }
+
+  onArchive() {
+    this.archive.emit(this._book);
+  }
+}
