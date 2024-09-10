2b6f231362bf821ec9efbd019146b973cddb4db5
Hubert Janiuk <hubert.janiuk@dajar.pl>
2024-09-10 20:36:22 +0200

    Borrowing a book implementation

--- book-network-ui/src/app/modules/book/pages/book-list/book-list.component.ts
+++ book-network-ui/src/app/modules/book/pages/book-list/book-list.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {BookService} from "../../../../services/services/book.service";
 import {Router} from "@angular/router";
 import {PageResponseBookResponse} from "../../../../services/models/page-response-book-response";
+import {BookResponse} from "../../../../services/models/book-response";
 
 @Component({
   selector: 'app-book-list',
@@ -12,6 +13,8 @@ export class BookListComponent implements OnInit{
   bookResponse: PageResponseBookResponse = {};
   page = 0;
   size = 5;
+  message = "";
+  level = "success";
 
   constructor(
     private bookService: BookService,
@@ -61,5 +64,22 @@ export class BookListComponent implements OnInit{
 
   get isLastPage(): boolean {
     return this.page == this.bookResponse.totalPages as number - 1
+  }
+
+  borrowBook(book: BookResponse) {
+    this.message = "";
+    this.bookService.borrowBook({
+      "book-id": book.id as number
+    }).subscribe({
+      next: () => {
+        this.level = "success";
+        this.message = 'Book successfully added to your list';
+      },
+      error: err => {
+        console.log(err);
+        this.level = "error";
+        this.message = err.error.error;
+      }
+    });
   }
 }
